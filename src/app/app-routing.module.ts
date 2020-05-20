7e6aba5227fca00419a080e494d5e56a68362f3e
PatrizioColomba <patrizio.colomba@gmail.com>
2020-05-20 15:42:11 +0200

    Skeleton customization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AngularComponent } from './angular/angular.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PortfolioComponent },
+  { path: 'angular', component: AngularComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

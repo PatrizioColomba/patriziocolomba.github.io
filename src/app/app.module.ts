4eb5734f6f32cdf1f6824fa94262f210514261bb
PatrizioColomba <patrizio.colomba@gmail.com>
2020-05-20 15:42:11 +0200

    Skeleton customization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AngularComponent } from './angular/angular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    AngularComponent
   ],
   imports: [
     BrowserModule,

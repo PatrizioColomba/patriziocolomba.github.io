482309f533907c71c99dcdb797f4642f6e2ef3e1
PatrizioColomba <patrizio.colomba@gmail.com>
2020-05-22 16:54:01 +0200

    Post page, toolbar component and robots.txt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  onResize(evt: Event) {
+    console.log(evt);
+  }
+}

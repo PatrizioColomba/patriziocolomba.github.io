d949f7e362e092219ca1be58b38a1b02bcc9ed54
PatrizioColomba <patrizio.colomba@gmail.com>
2020-05-23 15:11:59 +0200

    Adds temporary post for guests

--- src/app/base/post/paper.post.mock.ts
+++ src/app/base/post/paper.post.mock.ts
@@ -1,6 +1,13 @@
 import { Paper } from './paper.post';
 
 export const PAPERS: Paper[] = [
-    new Paper("Post di prova", "Questo è un modello di prova", new Date()),
-    new Paper("Post di prova 2", "Questo è un modello di prova 2", new Date())
+    new Paper(
+        "Pagina post nello stato attuale", 
+        "Il sito è ospitato interamente su Github Pages. Questo post è scritto nel vettore PAPERS per rendere le pagine fruibili nell'immediato.\n" +
+        "\n" +
+        "Nelle prossime settimane verrà implementato un servizio REST su host remoto per rendere il sistema professionale.\n" + 
+        "\n" +
+        "Il codice sorgente dell'applicazione, scritta utilizzando come base Angular 9, è visualizzabile nel repository raggiungibile al link in fondo alla pagina " +
+        "dove è possibile trovare il mio indizzo mail per eventuali contatti.\n\n", 
+        new Date(2020, 4, 23))
 ]

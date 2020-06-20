7e767605b26554720a0d6589d32fb7b1cc2d575d
PatrizioColomba <patrizio.colomba@gmail.com>
2020-06-20 16:03:46 +0200

    Changes favicon and fix security issues

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,15 @@
+import {firebase} from './firebase.environment';
+import {name, version} from '../../package.json';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
-  fireBase: {
-    apiKey: "AIzaSyCLG1v0xv6kKDpsNjJ3hWGLhHJ-U0uIH5M",
-    authDomain: "patriziocolomba-github.firebaseapp.com",
-    databaseURL: "https://patriziocolomba-github.firebaseio.com",
-    projectId: "patriziocolomba-github",
-    storageBucket: "patriziocolomba-github.appspot.com",
-    messagingSenderId: "448666273977",
-    appId: "1:448666273977:web:f512936e48e39e3158f368",
-    measurementId: "G-434SE74FTV"
-  }
+  firebase,
+  name,
+  version
 };
 
 /*

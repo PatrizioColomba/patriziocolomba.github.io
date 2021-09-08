3b26dc65194577044e56c39ac56209fab4250535
Patrizio Colomba <patrizio.colomba@gmail.com>
2021-09-08 16:27:03 +0200

    Clean dev branch to update new project files

--- src/app/base/post/post.ts
+++
@@ -1,9 +0,0 @@
-import { Source } from '../source/source';
-
-export interface Post {
-    getId(): number;
-    getTitle(): string;
-    getText(): string;
-    getCreated(): Date;
-    getCreatedStr(locale: string): string;
-}
--- src/app/services/fire-post.service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FirePost } from './fire-post.service';
-
-describe('FirePost', () => {
-  it('should create an instance', () => {
-    expect(new FirePost()).toBeTruthy();
-  });
-});

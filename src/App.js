8c10476f446d53b80c458c4ba4a938bef4f5d26b
Patrizio Colomba <patrizio.colomba@gmail.com>
2021-09-20 18:41:10 +0200

    Add rouring support (react-bootstrap and react-router-dom)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
 import Blog from './blog/Blog';
 import Dashboard from './dashboard/Dashboard';
+import { Route, Switch, HashRouter } from 'react-router-dom'
 import TopBar from './topbar/TopBar';
+import React from 'react';
 
 function App() {
   return (
-    <section>
-      <TopBar />
-      <Dashboard />
-      <Blog src="post.md"/>
-    </section>
+    <React.StrictMode>
+      <HashRouter>
+        <section>
+          <TopBar />
+          <Dashboard />
+          <Switch>
+            <Route exact path="/">
+              <Blog src="posts/post.md"/>
+            </Route>
+            <Route exact path="/kotlin">
+              <Blog src="posts/post.md"/>
+            </Route>
+            <Route exact path="/react">
+              <Blog src="posts/post.md"/>
+            </Route>
+            <Route exact path="/laravel">
+              <Blog src="posts/post.md"/>
+            </Route>
+            <Route exact path="/spring">
+              <Blog src="posts/post.md"/>
+            </Route>
+          </Switch>
+        </section>
+      </HashRouter>
+    </React.StrictMode>
   );
 }
 

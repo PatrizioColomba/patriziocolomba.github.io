8b80bd1c194c0a3b687524a7ff922df9234643e3
Patrizio Colomba <patrizio.colomba@gmail.com>
2021-09-20 18:41:10 +0200

    Add rouring support (react-bootstrap and react-router-dom)

--- src/box/Box.js
+++ src/box/Box.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './box.css'
 import '../vars.css'
+import { NavLink } from 'react-router-dom'
 
 class Box extends React.Component {
     render() {
         return (
-            <div className="card content">
-                <img src={this.props.logo} alt=""></img>
-                <div className="line"></div>
-                <div>{this.props.section}</div>
-            </div>
+            <NavLink to={this.props.url} activeClassName="selected">
+                <div className="content">
+                    <img src={this.props.logo} alt=""></img>
+                    <div className="line"></div>
+                    <div>{this.props.section}</div>
+                </div>
+            </NavLink>
         );
     }
 }

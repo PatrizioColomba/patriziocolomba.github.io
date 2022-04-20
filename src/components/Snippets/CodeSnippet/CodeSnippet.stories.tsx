e935abee01e48cd1c82790cbba8bd8aab9ed2992
Patrizio Colomba <patrizio.colomba@gmail.com>
2022-04-20 18:16:54 +0200

    Implement code syntax highlight
    Implement component CodeSnippet and code syntax highlight

---
+++ src/components/Snippets/CodeSnippet/CodeSnippet.stories.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import CodeSnippet from "./CodeSnippet";
+
+export default {
+  title: "CodeSnippet",
+};
+
+export const Default = () => <CodeSnippet text={""} lang={"kotlin"} />;
+
+Default.story = {
+  name: "default",
+};

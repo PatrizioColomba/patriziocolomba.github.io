6bb4d91b03e5ad59a39555fc20d02e2f0e6892f2
Patrizio Colomba <patrizio.colomba@gmail.com>
2022-04-20 18:16:54 +0200

    Implement code syntax highlight
    Implement component CodeSnippet and code syntax highlight

--- src/components/Snippets/Snippets.tsx
+++ src/components/Snippets/Snippets.tsx
@@ -1,12 +1,27 @@
-import React, { FC } from 'react';
+import { Firebase } from "App";
+import { Block } from "baseui/block";
+import { useFetch } from "components/useFetch";
+import { useState } from "react";
+import CodeSnippet from "./CodeSnippet/CodeSnippet";
 
+export type CodeLang = "kotlin" | "js" | "rust" | "java";
+interface SnippetsData {
+  lang: CodeLang;
+  text: string;
+}
+export default function Snippets() {
+  const [snippets, setSnippets] = useState<SnippetsData[]>([]);
 
-interface SnippetsProps {}
+  useFetch(Firebase.Snippets, []).then(setSnippets);
 
-const Snippets: FC<SnippetsProps> = () => (
-  <div data-testid="Snippets">
-    Snippets Component
-  </div>
-);
-
-export default Snippets;
+  return (
+    <div data-testid="Snippets">
+      {snippets.map((s, i) => (
+        <Block padding={"2em"}>
+          <h2>Code Snippet in {s.lang}</h2>
+          <CodeSnippet key={`snippet-${i}`} text={s.text} lang={s.lang} />
+        </Block>
+      ))}
+    </div>
+  );
+}

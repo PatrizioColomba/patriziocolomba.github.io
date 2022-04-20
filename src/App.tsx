dd83fde8971fee665c6ef8cabc3eabaefe265cff
Patrizio Colomba <patrizio.colomba@gmail.com>
2022-04-20 12:59:27 +0200

    Implement TopNavigation component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AppProvider } from "components/AppContext";
 import Dashboard from "components/Dashboard/Dashboard";
 import Snippets from "components/Snippets/Snippets";
 import Stack from "components/Stack/Stack";
+import TopNavigation from "components/TopNavigation/TopNavigation";
 import { Outlet, ReactLocation, Router } from "react-location";
 export enum Firebase {
   Stacks = "https://patriziocolomba-c5ef8.firebaseio.com/stacks.json",
@@ -28,6 +29,7 @@ export default function App() {
     <Router location={location} routes={routes}>
       <AppProvider>
         <Block backgroundImage="url(/assets/images/banner.jpg)" backgroundPosition="center" backgroundSize="cover" height="20em" />
+        <TopNavigation position="sticky" top={0} />
         <Block marginTop={"1em"}>
           <Outlet />
         </Block>

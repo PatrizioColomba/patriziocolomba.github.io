c0ba9270f17646b3c9e5fd78df302b104b43a6f2
Patrizio Colomba <patrizio.colomba@gmail.com>
2024-07-08 19:11:11 +0200

    next.js skeleton

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    config.watchOptions = {
-      poll: 1000, 
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
-}
+const nextConfig = {};
 
 export default nextConfig;

7c0f50f692dfbd56c1e1485ea983416a569747d5
Patrizio Colomba <patrizio.colomba@gmail.com>
2024-07-09 20:19:18 +0200

    fix(hot-reload): hot reload for docker container

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+ const nextConfig = {
+   webpack: (config, { isServer }) => {
+     // Aggiungi le opzioni di watch
+     config.watchOptions = {
+       poll: 1000, // Intervallo di polling in millisecondi
+       aggregateTimeout: 300, // Timeout di aggregazione
+     };
+
+     return config;
+   },
+ }
 
 export default nextConfig;

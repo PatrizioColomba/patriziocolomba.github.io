a90af53a801a53383deb3afdf004dce27ce14e41
Patrizio Colomba <patrizio.colomba@gmail.com>
2024-07-10 19:15:29 +0200

    build(gh-pages-deploy): impl

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
- const nextConfig = {
-   webpack: (config, { isServer }) => {
-     config.watchOptions = {
-       poll: 1000, 
-       aggregateTimeout: 300,
-     };
-
-     return config;
-   },
- }
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    config.watchOptions = {
+      poll: 1000, 
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+}
 
 export default nextConfig;

fc37c86e825f209a84ae0064e8c35ed35cc24b70
Patrizio Colomba <patrizio.colomba@gmail.com>
2024-12-09 20:20:09 +0100

    font change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import "@fontsource/space-mono";
+import "@fontsource/space-mono/400.css";
+import "@fontsource/space-mono/400-italic.css";
 
 export const metadata: Metadata = {
   title: "patriziocolomba.github.io",

e093f237aaecf7da26e65c1d0c55c70ab048eec3
Patrizio Colomba <patrizio.colomba@gmail.com>
2024-12-26 16:43:18 +0000

    refactor(layout): update title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import "@fontsource/space-mono/400.css";
 import "@fontsource/space-mono/400-italic.css";
 
 export const metadata: Metadata = {
-  title: "patriziocolomba.github.io",
-  description: "Patrizio Colomba on GitHub Pages",
+  title: "Patrizio Colomba's Log",
+  description: "Patrizio Colomba's Log",
 };
 
 export default function RootLayout({

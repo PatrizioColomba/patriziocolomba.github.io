c87bbb0b1a0fce2b054f5fd1645d110aad2c3ced
Patrizio Colomba <patrizio.colomba@gmail.com>
2025-01-01 15:12:02 +0000

    feat(page): fetch and display data from external API on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,26 @@
 
 import { Grid2, Typography } from "@mui/material";
 import AppTheme from "./AppTheme";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState<Data[]>([]);
+
+  useEffect(() => {
+    GetTest().then(setData).catch(console.error);
+  }, []);
+
+  async function GetTest(): Promise<Data[]> {
+    const response = await fetch('https://patriziocolomba-web-app.azurewebsites.net/Test');
+    const data = await response.json();
+    return data
+  }
+
   return (<AppTheme><main><Grid2 sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-    <Typography>A simple text test.</Typography>
+    { data.map(item => (<Typography>{item.data}</Typography>))}
   </Grid2></main></AppTheme>);
 }
+
+type Data = {
+  data: string;
+};
